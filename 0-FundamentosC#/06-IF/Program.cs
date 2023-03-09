39e0512714403a1627a8a2c2f6cc3feee3bf9d92
Wesley Martins <wesley.mlopes@castgroup.com>
2023-03-09 06:41:14 -0300

    operador de comparação IF

---
+++ 0-FundamentosC#/06-IF/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _06_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UTILIZANDO O IF
+            if (32 == 32)
+                Console.WriteLine("Sim");
+            
+            else
+                Console.WriteLine("Não");
+        }
+    }
+}
