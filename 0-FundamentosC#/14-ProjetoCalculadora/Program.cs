800eb1666d1d7902fb8a0573990787d9b431e954
Wesley Martins <wesley.mlopes@castgroup.com>
2023-03-22 07:36:07 -0300

    Começando o projeto de uma calculadora

---
+++ 0-FundamentosC#/14-ProjetoCalculadora/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _14_ProjetoCalculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Projeto de uma Calculadora para fixar os conteúdos estudados:
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo Valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+            float resultado = valor1 + valor2;
+            Console.WriteLine("Resultado da soma dos números é: " + resultado);
+            Console.WriteLine($"Resultado da soma, usando INTERPOLAÇÃO '$': {resultado}");
+        }
+    }
+}
